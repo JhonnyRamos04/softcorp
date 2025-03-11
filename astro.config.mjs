f685146fe345a26141cfbb9014ba18b18d11e7d4
JhonnyRamos04 <onlyjhonny01@gmail.com>
2025-03-11 17:47:14 -0400

    se agrego config para hacer deploy en vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,14 +6,19 @@ import icon from "astro-icon";
 
 import react from "@astrojs/react";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://astroship.web3templates.com",
   output: 'server',
   integrations: [tailwind(), mdx(), sitemap(), icon(), react()],
+
   vite: {
     ssr: {
       noExternal: ["astro", "@astrojs/"],
     },
-  }
+  },
+
+  adapter: vercel()
 });
